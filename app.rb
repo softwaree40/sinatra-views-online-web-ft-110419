a5b756d9a592e8fcaf9e3ff556a7a325df90a6b9
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-21 04:32:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
 	get '/' do
 	  erb :index
 		
@@ -12,3 +13,10 @@ class App < Sinatra::Base
 		
 	end
 end
+=======
+	 get '/' do
+	    erb :index
+	 end
+	 
+end 
+>>>>>>> 1d8100559e2b1df46a512ccf33d27bea0767590d
